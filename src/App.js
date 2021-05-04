e20b92d24b7d4ac2a0bf9aa0a7345d2c43d9afe6
pasupathi.thangavel <pasupathi.thangavel@ameexusa.com>
2021-05-04 16:56:54 +0530

    env files created and env variable use in react component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {process.env.REACT_APP_VARIABLENAME}
         <a
           className="App-link"
           href="https://reactjs.org"
